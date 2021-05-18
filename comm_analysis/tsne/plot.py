5f02b954ea78f18753b437168499e07d462a7603
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-05-18 15:17:52 +0530

    updated tsne and plots

---
+++ comm_analysis/tsne/plot.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt, numpy as np, os, argparse 
+
+
+def plot(embedded_filename, messages_filename, save=False): 
+    X_embedded = np.load(embedded_filename) 
+    print(X_embedded.shape) 
+    messages = np.load(messages_filename) 
+
+    x=[i[0] for i in X_embedded]
+    y=[i[1] for i in X_embedded]
+    z=[i[0] for i in messages[:len(X_embedded)]] 
+    plt.scatter(x,y, c=z) 
+    
+    where = "/".join(embedded_filename.split('/')[:-1])+"/plots" 
+    if not os.path.exists(where): os.makedirs(where) 
+    if save: plt.savefig(os.path.join(where, "all.png")) 
+    else: plt.show() 
+    plt.close() 
+
+    for j in range(25):     
+        x=[X_embedded[i][0] for i in range(j, len(X_embedded), 25)] 
+        y=[X_embedded[i][1] for i in range(j, len(X_embedded), 25)] 
+        z=[messages[i][0] for i in range(j, len(X_embedded), 25)] 
+        sc = plt.scatter(x,y, c=z, cmap='RdYlBu') 
+        plt.colorbar(sc) 
+        if save: plt.savefig(os.path.join(where, "timestep_"+str(j+1)+".png")) 
+        else: plt.show() 
+        plt.close() 
+    
+
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser() 
+    parser.add_argument("--filepath", type=str, help="eval states and messages (path)") 
+    args = parser.parse_args() 
+    plot(
+        embedded_filename=os.path.join(args.filepath, "embedded_states.npy"),
+        messages_filename=os.path.join(args.filepath, "hammer_messages.npy"),
+        save=True 
+    ) 
--- comm_analysis/tsne/tsne.py
+++ comm_analysis/tsne/tsne.py
@@ -1,9 +1,18 @@
-import numpy as np 
+import numpy as np, argparse 
 from sklearn.manifold import TSNE 
 
-X = np.array([[0, 0, 0, 0, 0], [0, 1, 1, 1, 1], [1, 0, 1, 0, 1], [1, 1, 1, 1, 1]]) 
-print(X.shape)
-X_embedded = TSNE(n_components=2).fit_transform(X) 
-print(X_embedded.shape) 
+def tsne(filename): 
+    X = np.load(filename, mmap_mode="r") 
+    print(X.shape) 
 
-print(X_embedded) 
+    X_embedded = TSNE(n_components=2).fit_transform(X) 
+    print(X_embedded.shape) 
+    np.save("/".join(filename.split('/')[:-1])+"/embedded_states.npy", X_embedded) 
+
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser() 
+    parser.add_argument("--filename", type=str, help="eval states") 
+    args = parser.parse_args() 
+
+    tsne(filename=args.filename) 
+
