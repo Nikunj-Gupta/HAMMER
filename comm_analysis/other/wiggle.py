b03c3d73fb1b51d50900eb22cb120be73e00d512
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-05-19 20:56:10 +0530

    added for loops to calculate multiple combinations of plots

--- comm_analysis/other/wiggle.py
+++ comm_analysis/other/wiggle.py
@@ -1,8 +1,7 @@
-import argparse, os, numpy as np, torch, json
+import argparse, os, numpy as np, torch, matplotlib.pyplot as plt 
+
 from time import sleep
 from pettingzoo.mpe import simple_spread_v2 
-from pettingzoo.sisl import multiwalker_v6 
-import matplotlib.pyplot as plt, numpy as np, os, argparse 
 
 from hammer import PPO 
 from utils import read_config 
@@ -18,29 +17,19 @@ def preprocess_obs(obs, limit):
     return obs 
 
 def run(args): 
-    if args.envname == "cn": 
-        env = simple_spread_v2.parallel_env(N=args.nagents, local_ratio=0.5, max_cycles=args.maxcycles) 
-        env.reset()
-        agents = [agent for agent in env.agents] 
-        if args.heterogeneity: 
-            obs_dim = len(preprocess_one_obs(env.reset(), limit=args.limit)["agent_0"]) 
-        elif args.partialobs:
-            obs_dim = len(preprocess_obs(env.reset(), limit=args.limit)["agent_0"]) 
-        else:
-            obs_dim = env.observation_spaces[env.agents[0]].shape[0]
+    env = simple_spread_v2.parallel_env(N=args.nagents, local_ratio=0.5, max_cycles=args.maxcycles) 
+    env.reset()
+    agents = [agent for agent in env.agents] 
+    if args.heterogeneity: 
+        obs_dim = len(preprocess_one_obs(env.reset(), limit=args.limit)["agent_0"]) 
+    elif args.partialobs:
+        obs_dim = len(preprocess_obs(env.reset(), limit=args.limit)["agent_0"]) 
+    else:
+        obs_dim = env.observation_spaces[env.agents[0]].shape[0]
 
-        action_dim = env.action_spaces[env.agents[0]].n 
-        agent_action_space = env.action_spaces[env.agents[0]] 
+    action_dim = env.action_spaces[env.agents[0]].n 
+    agent_action_space = env.action_spaces[env.agents[0]] 
     
-    elif args.envname == "mw": 
-        env = multiwalker_v6.parallel_env(n_walkers=args.nagents) 
-        env.reset()
-        agents = [agent for agent in env.agents] 
-        obs_dim = env.observation_spaces[env.agents[0]].shape[0]        
-        action_dim = env.action_spaces[env.agents[0]].shape[0] 
-        agent_action_space = env.action_spaces[env.agents[0]]
-
-
     config = read_config(args.config) 
     if not config:
         print("config required")
@@ -53,8 +42,6 @@ def run(args):
         torch.manual_seed(random_seed)
         np.random.seed(random_seed)
     
-    
-
     betas = (0.9, 0.999)
 
     HAMMER = PPO(
@@ -77,13 +64,6 @@ def run(args):
     if args.eval: 
         HAMMER.load(os.path.realpath(args.eval_path))
 
-    if args.dru_toggle: 
-        print("Using DRU") 
-    else: 
-        print("Not Using DRU")
-
-    # logging variables
-
     if args.heterogeneity: 
         print('ERROR')
         obs = preprocess_one_obs(env.reset(), limit=args.limit) 
@@ -93,38 +73,53 @@ def run(args):
     else:  
         obs = env.reset() 
 
-    
     W1 = 7 
     W2 = 8 
 
-    # For checking 
-    X = np.load(os.path.join("saves/save/eval--env_cn--n_3--dru_0--meslen_1--sharedparams_1--randomseed_9/dataset/hammer_states.npy"), mmap_mode="r") 
-    print(min(X[:,W1])) 
-    print(max(X[:,W1])) 
-    print(min(X[:,W2])) 
-    print(max(X[:,W2])) 
+    # # For checking 
+    # X = np.load(os.path.join("saves/save/eval--env_cn--n_3--dru_0--meslen_1--sharedparams_1--randomseed_9/dataset/hammer_states.npy"), mmap_mode="r") 
+    # print(min(X[:,W1])) 
+    # print(max(X[:,W1])) 
+    # print(min(X[:,W2])) 
+    # print(max(X[:,W2])) 
     
-    # exit()
+    # # exit()
 
-    x = [] 
-    y = [] 
-    z = [] 
-    for _ in range(10000):
-        obs['agent_0'][W1] = np.random.uniform(-2, 2)
-        obs['agent_0'][W2] = np.random.uniform(-2, 2)
+    where = "plots/cn"+"/wiggle_plots" 
+    if not os.path.exists(where): os.makedirs(where) 
+    
+    for agent in agents: 
+        for i in range(0, 18, 2): 
+            for message_index in range(3): 
+                W1 = i 
+                W2 = i+1 
+                x = [] 
+                y = [] 
+                z = [] 
 
-        actions, messages = HAMMER.policy_old.act(obs, HAMMER.memory, HAMMER.global_memory)  
-        
-        x.append(HAMMER.global_memory.states[0][0, W1])
-        y.append(HAMMER.global_memory.states[0][0, W2])
-        message_index = 0 
-        z.append(HAMMER.global_memory.messages[0][message_index][0])
-        HAMMER.global_memory.clear_memory()
-    sc = plt.scatter(x,y, c=z, cmap='RdYlBu') 
-    plt.colorbar(sc) 
-    plt.show() 
-    sleep(4)
-    plt.close() 
+                for _ in range(10000):
+                    obs[agent][W1] = np.random.uniform(-2, 2)
+                    obs[agent][W2] = np.random.uniform(-2, 2)
+
+                    actions, messages = HAMMER.policy_old.act(obs, HAMMER.memory, HAMMER.global_memory)  
+                    
+                    x.append(HAMMER.global_memory.states[0][0, (18*int(agent[-1]))+W1])
+                    y.append(HAMMER.global_memory.states[0][0, (18*int(agent[-1]))+W2])
+                    # message_index = 0 
+                    z.append(HAMMER.global_memory.messages[0][message_index][0])
+                    HAMMER.global_memory.clear_memory()
+
+                sc = plt.scatter(x,y, c=z, cmap='RdYlBu') 
+                plt.colorbar(sc) 
+                name = "--".join([
+                    agent, 
+                    "W1_"+str(W1), 
+                    "W2_"+str(W2), 
+                    "message_index_"+str(message_index) 
+                ])
+                plt.savefig(os.path.join(where, name+".png")) 
+                # plt.show() 
+                plt.close() 
         
 
 if __name__ == '__main__':
