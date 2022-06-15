426c1d32dc3e5f9ed18306a506c7b0705b8be16c
Nikunj Gupta <ng2531@nyu.edu>
2022-06-15 00:29:54 -0400

    added run_utils and runs

---
+++ run_utils.py
@@ -0,0 +1,52 @@
+import os, yaml 
+from pathlib import Path 
+from itertools import count 
+
+dumpdir = "runs/" 
+if not os.path.isdir(dumpdir):
+    os.mkdir(dumpdir)
+fixed_text = "#!/bin/bash\n"\
+             "#SBATCH --nodes=1\n"\
+             "#SBATCH --cpus-per-task=16 \n"\
+             "#SBATCH --time=10:00:00\n"\
+             "#SBATCH --mem=40GB\n"\
+             "#SBATCH --gres=gpu:1\n"
+
+
+config_file = "configs/mw.yaml"
+
+with open(config_file, "r") as stream:
+        try: config = yaml.safe_load(stream) 
+        except yaml.YAMLError as exc: print(exc) 
+
+
+
+for dru_toggle in [0, 1]: 
+    for meslen in [0, 1, 2, 3]: 
+        for randomseed in [14712, 10453, 92959, 61033, 90300]: 
+            exp = str(randomseed) 
+            command = fixed_text + "#SBATCH --job-name="+exp+"\n#SBATCH --output="+exp+".out\n"
+            command += "\nsource ../venvs/hammer/bin/activate\n"\
+                "\nmodule load python/intel/3.8.6\n"\
+                "module load openmpi/intel/4.0.5\n"\
+                "time python3 hammer-run.py " 
+            command = ' '.join([
+                command, 
+                '--envname mw', 
+                '--config configs/mw.yaml', 
+                '--nagents 3', 
+                '--dru_toggle', str(dru_toggle), 
+                '--meslen', str(meslen), 
+                '--randomseed', str(randomseed), 
+                ]) 
+            # print(command) 
+            log_dir = Path(dumpdir)
+            for i in count(1):
+                temp = log_dir/('run{}.sh'.format(i)) 
+                if temp.exists():
+                    pass
+                else:
+                    with open(temp, "w") as f:
+                        f.write(command) 
+                    log_dir = temp
+                    break 
